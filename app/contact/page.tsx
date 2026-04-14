093f18c07388730a61a15eca1c8a5a5e2f887db4
Renz-Victa <victarenz472@gmail.com>
2026-04-14 11:24:00 +0200

    Changed the business name to Sawrus

--- app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from 'next'
-import ContactClient from './ContactClient'
-export const metadata: Metadata = {
-  title: 'Book a Call',
-  description: 'Schedule a free 30-minute strategy call with AsleepTurtle.',
-}
-export default function ContactPage() { return <ContactClient /> }
